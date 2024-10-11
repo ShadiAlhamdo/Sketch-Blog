1c2fcbe11f8e2436bba8606c4802a6569abc09c8
Shadi Alhamdo <shadi998@gmail.com>
2024-10-11 20:39:22 +0300

    Server Url Editing

--- frontend/src/Utils/requrst.js
+++ frontend/src/Utils/requrst.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const request=axios.create({
-    baseURL:"http://localhost:8080"
+    baseURL:"https://sketch-blog-server-rho.vercel.app/"
 });
 
 export default request
