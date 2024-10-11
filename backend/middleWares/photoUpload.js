88d4f751f5f1577b00cb206b6271d79d27c810a4
Shadi Alhamdo <shadi998@gmail.com>
2024-10-11 23:18:23 +0300

    Server Url Editing 10

--- backend/middleWares/photoUpload.js
+++ backend/middleWares/photoUpload.js
@@ -1,35 +1,6 @@
-const path=require("path");
-const multer=require("multer");
+const multer = require('multer');
 
-// Photo Stoarge    
-const photostorage=multer.diskStorage({
-    destination:function(req,file,callback){
-        callback(null,path.join(__dirname,"../images"));
-    },
-    filename:function(req,file,callback){
-        if(file){
-            callback(null,new Date().toISOString().replace(/:/g,"-")+file.originalname);
-        }
-        else{
-            callback(null,false);
-        }
-    }
-});
-
-// Photo Upload Middle ware
-
-const PhotoUpload=multer({
-    storage:photostorage,
-    fileFilter:function(req,file,cb){
-        if(file.mimetype.startsWith("image")){
-            cb(null,true)
-        }
-        else{
-            cb({message:"Unsupported File Format"},false)
-        }
-    },
-    limits:{fieldSize: 1024*1024} //1 megabyte
-});
-
+const storage = multer.memoryStorage();
+const PhotoUpload = multer({ storage });
 
 module.exports=PhotoUpload;
--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -22,6 +22,6 @@ router.route("/count").get(verifytokenAdmin,getUsersCountCtrl);
 
 // /api/users/profile
 router.route("/profile/profile-photo-upload")
-.post(verifyToken,profilePhotoUploadCtrl);
+.post(verifyToken,PhotoUpload.single("image"),profilePhotoUploadCtrl);
 
 module.exports=router;
