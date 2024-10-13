d4930d585e91643af7b2fa0fff23a353cca0ffe0
Shadi Alhamdo <shadi998@gmail.com>
2024-10-13 17:55:58 +0300

    Configure Pagenation 2

--- frontend/src/Components/Pagenation/Pagenation.jsx
+++ frontend/src/Components/Pagenation/Pagenation.jsx
@@ -1,9 +1,12 @@
+import { useEffect } from 'react';
 import './Pagenation.css'
 const Pagenation = ({pages,currentPage,setCurrentPage}) => {
+   useEffect(()=>{
     const generatedPages=[];
     for(let i=1 ;i<=pages;i++){
         generatedPages.push(i);
     }
+   },[])
     return (  
         <div className="pagenation">
             <button
